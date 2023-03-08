f6c76ad9c4e59ae0cb287b4e73423d1f5bbda9da
pierr05 <pierreachlet322@gmail.com>
2023-03-08 10:01:58 -0500

    added code to index.js file

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is your project description?'
+    },
+    {
+        type: 'input',
+        name: 'contents',
+        message: 'What is your project table of contents?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the use of for your project?'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What is the license for your project?'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can other developers contribute to your project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are some ways to run test on your application?'
+    }
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+])
+.then(function(data) {
+    
+    fs.writeFile('README.md',`${generateMarkdown(data)}`, (error) => {
+        error ? console.log(error) : console.log("README.md file is successfully created!")
+    })
+})
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Include packages needed for this application
 
-// Function call to initialize app
-init();
+// // TODO: Create an array of questions for user input
+// const questions = [];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
